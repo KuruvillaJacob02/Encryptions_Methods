81dd72fd3542b537db6be3cab13ef8f1c96ee728
Sayak Naskar <sayaknaskar@gmail.com>
2020-10-03 19:55:04 +0530

    Update Reverse_Cipher.py

--- Reverse_Cipher.py
+++ Reverse_Cipher.py
@@ -1,10 +1,10 @@
 # message = 'I love to code in python.'
-message = input("Please type your message: ")
-translated = ''
+def Reverse_cipher(message):
+   translated = ''
+   i = len(message) - 1
+      while i >= 0:
+         translated = translated + message[i]
+         i = i - 1
+      s = "The cipher text is : "+translated
+   return s
 
-i = len(message) - 1
-
-while i >= 0:
-   translated = translated + message[i]
-   i = i - 1
-print("The cipher text is : ", translated)
